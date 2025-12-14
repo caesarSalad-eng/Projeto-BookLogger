59eebf90085805513525dca8071c622eebb5e4e6
Kendall Ycaro <ycaroinacio203@gmail.com>
2025-12-14 17:39:14 -0300

    Resolvendo Problema no Codigo anterior

---
+++ Projeto-BookLogger/src/main/java/com/br/Projeto_BookLogger/ProjetoBookLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.br.Projeto_BookLogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBookLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBookLoggerApplication.class, args);
+	}
+
+}
---
+++ Projeto-BookLogger/src/test/java/com/br/Projeto_BookLogger/ProjetoBookLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Projeto_BookLogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBookLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
